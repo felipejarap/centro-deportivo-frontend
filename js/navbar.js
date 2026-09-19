6efa9a14c668ef0d2b72b0ac211d5ea6eba9ca49
julian-duocuc <juli.duocuc.cl>
2026-09-19 00:08:11 -0300

    Se reestablece navbar duplicada

--- js/navbar.js
+++
@@ -1,7 +0,0 @@
-fetch('/html/navbar.html')
-    .then(function(response) {
-        return response.text();
-    })
-    .then(function(data) {
-        document.getElementById('navbar-container').innerHTML = data;
-    });
